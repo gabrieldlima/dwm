7783f6c1bf845c3932de2ee64b2a78b0fa884c77
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-12-11 21:07:14 -0300

    Don't remove border and gap if layout is monocle or only one client

--- dwm.c
+++ dwm.c
@@ -1291,16 +1291,9 @@ resizeclient(Client *c, int x, int y, int w, int h)
 	if (c->isfloating || c->mon->lt[c->mon->sellt]->arrange == NULL) {
 		gapincr = gapoffset = 0;
 	} else {
-		/* Remove border and gap if layout is monocle or only one client */
-		if (c->mon->lt[c->mon->sellt]->arrange == monocle || n == 1) {
-			gapoffset = 0;
-			gapincr = -2 * borderpx;
-			wc.border_width = 0;
-		} else {
-			gapoffset = gappx;
-			gapincr = 2 * gappx;
-		}
-	}
+        gapoffset = gappx;
+        gapincr = 2 * gappx;
+    }
 
 	c->oldx = c->x; c->x = wc.x = x + gapoffset;
 	c->oldy = c->y; c->y = wc.y = y + gapoffset;
