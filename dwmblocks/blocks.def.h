076ad697f84b07148e823f5f82961e8ca28453dc
Gabriel Lima <gabrielgsdlima@gmail.com>
2022-12-31 16:12:11 -0300

    Scripts for dwmblocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,9 +1,15 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "$HOME/.local/bin/clock",					5,		0},
+    /*Icon*/ /*Command*/                              /*Update Interval*/ /*Update Signal*/
+    {"  ",  "$HOME/.local/bin/dwmblocks/dwm-kernel", 3600,               0},
+    {"﬙ ",   "$HOME/.local/bin/dwmblocks/dwm-memory", 1,                  0},
+    {" ",   "$HOME/.local/bin/dwmblocks/dwm-temp",   5,                  0},
+    {" ",   "$HOME/.local/bin/dwmblocks/dwm-pacman", 600,                0},
+    {"墳 ",   "$HOME/.local/bin/dwmblocks/dwm-volume", 0,                10},
+    {" ",   "$HOME/.local/bin/dwmblocks/dwm-date",	  3600,               0},
+    {" ",   "$HOME/.local/bin/dwmblocks/dwm-clock",  60,                 0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+// Sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
