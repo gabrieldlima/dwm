cbe06924f24a3ebcf8b1d5aa9c76f561294f4d6f
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-01-19 20:56:15 -0300

    New scripts for dwmblocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,15 +1,18 @@
+#define PATH(name) "/home/gabriel/.local/bin/statusbar/"name
+
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/ /*Command*/                              /*Update Interval*/ /*Update Signal*/
-    {"  ",  "$HOME/.local/bin/dwmblocks/dwm-kernel", 3600,               0},
-    {"﬙ ",   "$HOME/.local/bin/dwmblocks/dwm-memory", 1,                  0},
-    {" ",   "$HOME/.local/bin/dwmblocks/dwm-temp",   5,                  0},
-    {" ",   "$HOME/.local/bin/dwmblocks/dwm-pacman", 600,                0},
-    {"墳 ",   "$HOME/.local/bin/dwmblocks/dwm-volume", 0,                10},
-    {" ",   "$HOME/.local/bin/dwmblocks/dwm-date",	  3600,               0},
-    {" ",   "$HOME/.local/bin/dwmblocks/dwm-clock",  60,                 0},
+    /*Icon*/ /*Command*/        /*Update Interval*/ /*Update Signal*/
+    {" ",    PATH("sb-kernel"), 600,                0},
+    {"",     PATH("sb-cpu"),    1,                  0},
+    {"",     PATH("sb-memory"), 1,                  0},
+    {"",     PATH("sb-temp"),   1,                  0},
+    {"",     PATH("sb-pacman"), 600,                0},
+    {"",     PATH("sb-volume"), 1,                  0},
+    {"",     PATH("sb-date"),   60,                 0},
+    {"",     PATH("sb-clock"),  60,                 0},
 };
 
 // Sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "  ";
+static char delim[] = " | ";
 static unsigned int delimLen = 5;
