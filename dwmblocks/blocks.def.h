ae701d99aaf480c0f8d2843dc075ef95873d5092
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-01-22 16:27:08 -0300

    New scripts

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,16 +1,16 @@
-#define PATH(name) "/home/gabriel/.local/bin/statusbar/"name
-
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/ /*Command*/        /*Update Interval*/ /*Update Signal*/
-    {" ",    PATH("sb-kernel"), 600,                0},
-    {"",     PATH("sb-cpu"),    1,                  0},
-    {"",     PATH("sb-memory"), 1,                  0},
-    {"",     PATH("sb-temp"),   1,                  0},
-    {"",     PATH("sb-pacman"), 600,                0},
-    {"",     PATH("sb-volume"), 0,                  10},
-    {"",     PATH("sb-date"),   60,                 0},
-    {"",     PATH("sb-clock"),  60,                 0},
+    /*Icon*/ /*Command*/    /*Update Interval*/ /*Update Signal*/
+    {" ",    "sb-kernel",   600,                 0},
+    {"",     "sb-cpu",      1,                   0},
+    {"",     "sb-memory",   1,                   0},
+    {"",     "sb-temp",     1,                   0},
+    {"",     "sb-disk",     60,                  0},
+    {"",     "sb-pacman",   600,                 0},
+    {"",     "sb-keyboard", 0,                  11},
+    {"",     "sb-volume",   0,                  10},
+    {"",     "sb-date",     60,                  0},
+    {"",     "sb-clock",    60,                  0},
 };
 
 // Sets delimeter between status commands. NULL character ('\0') means no delimeter.
