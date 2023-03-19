c064b030bbfe37109ec99d74f6c59302514c0713
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-03-19 11:37:56 -0300

    Modified font

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const unsigned int gappx     = 10;   /* gaps between windows */
 static const unsigned int snap      = 32;   /* snap pixel */
 static const int showbar            = 1;    /* 0 means no bar */
 static const int topbar             = 1;    /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
+static const char *fonts[]          = { "FiraCode Nerd Font:size=10" };
 static const char *colors[][3]      = {
     /*               fg           bg           border         */
     [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
